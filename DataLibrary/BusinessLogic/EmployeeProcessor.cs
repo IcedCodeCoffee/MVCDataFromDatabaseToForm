4401fb23ccdcc048992df38f3248a2a2da7bbfd2
IcedCodeCoffee <wolf.alexander1981@gmail.com>
2021-05-15 14:27:20 +0200

    Add project files.

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,38 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstName,
+            string lastName, string emailAdress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAdress = emailAdress
+            };
+
+            string sql = @"insert into dbo.Employee (EmployeeId, FirstName, LastName, EmailAdress)
+                           values (@EmployeeId, @FirstName, @LastName, @EmailAdress);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"select Id, EmployeeId, FirstName, LastName, EmailAdress
+                           from dbo.Employee;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name = "Employee ID")]
+        [Range( 100, 999, ErrorMessage ="Please type in a 3 digit number.")]
+        public int EmployeeID { get; set; }
+        
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please type in your first name.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please type in your last name.")]
+        public string LastName{ get; set; }
+        
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Adress")]
+        [Required(ErrorMessage = "Please type in your Email Adress.")]
+        public string EmailAdress { get; set; }
+        
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAdress", ErrorMessage = "Please match Email and Confirm Email.")]
+        public string ConfirmEmail { get; set; }
+        
+        [Display(Name = "Password")]
+        [Required(ErrorMessage ="Please type in a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Please type in an at least 5 character long password.")]
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Please match password and Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
